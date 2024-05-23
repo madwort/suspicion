c6343628521f5789953f72240944d52dcfd1440f
Tom Ward <tomward@fmail.co.uk>
2024-05-23 10:59:03 +0100

    Demo a simple use-case in a test

--- calculator/tests/test_calculate.py
+++ calculator/tests/test_calculate.py
@@ -4,9 +4,16 @@ from calculator.calculator1 import calculate as c1
 from calculator.calculator2 import calculate as c2
 from suspicion import find_failing_example
 
+from calculator.nodes import Add, Subtract, Value
 
 def test_calculate():
     assert find_failing_example(check) is None
+
+
+def test_simple():
+    expr = Add([Value(1), Value(2)])
+    check(expr)
+    assert c1(expr) == 3
 
 
 def check(expr):
