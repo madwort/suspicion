601fa86391dd54b2d5369eaefeebbc667a005eeb
Tom Ward <tomward@fmail.co.uk>
2024-05-23 10:59:24 +0100

    Lots of development
    * Lots of work on the tester
      * function flags for Binary/Nary nodes
      * lots of general dev
    * Add some more things to find in the calculator

--- calculator/calculator1.py
+++ calculator/calculator1.py
@@ -23,25 +23,20 @@ def visit_value(expr):
 
 @visit.register(nodes.Add)
 def visit_add(expr):
-    # this is found
-    if isinstance(expr.args[0], nodes.Add):
-        if isinstance(expr.args[0].args[0], nodes.Add):
-            if isinstance(expr.args[0].args[0].args[0], nodes.Add):
-                raise Exception("Four Adds in a row are verboten plz!", expr)
+    # # this is found
+    # if isinstance(expr.args[0], nodes.Add):
+    #     if isinstance(expr.args[0].args[0], nodes.Add):
+    #         if isinstance(expr.args[0].args[0].args[0], nodes.Add):
+    #             raise Exception("Four Adds in a row are verboten plz!", expr)
 
-    # this is only found by test_hypothesis_head tests
-    if isinstance(expr.args[0], nodes.Add):
-        if isinstance(expr.args[0].args[0], nodes.Divide):
-            if isinstance(expr.args[0].args[0].rhs, nodes.Add):
-                raise Exception("No nesting combo Add-Add-Div-Add plz!", expr)
-
-    # harder version
-    # hypothesis only finds this if it doesn't find the others
-    if len(expr.args) > 5 and isinstance(expr.args[1], nodes.Add):
-        raise Exception("Moar args plz!", expr)
+    # # this is only found by test_hypothesis_head tests
+    # if isinstance(expr.args[0], nodes.Add):
+    #     if isinstance(expr.args[0].args[0], nodes.Divide):
+    #         if isinstance(expr.args[0].args[0].rhs, nodes.Add):
+    #             raise Exception("No nesting combo Add-Add-Div-Add plz!", expr)
 
     # this test requires rotating the Add arguments
-    if isinstance(expr.args[1], nodes.Add):
+    if len(expr.args) > 1 and isinstance(expr.args[1], nodes.Add):
         raise Exception("Rotate your owl plz!", expr)
 
     return reduce(operator.add, (visit(arg) for arg in expr.args))
@@ -49,6 +44,17 @@ def visit_add(expr):
 
 @visit.register(nodes.Multiply)
 def visit_multiply(expr):
+    # # harder version
+    # if len(expr.args) > 5 and isinstance(expr.args[1], nodes.Multiply):
+    #     raise Exception("Moar args plz!", expr)
+
+    # # even harder version
+    # # this is findable with some rules commented out, add_a_nary_node limited to
+    # # multiply & max_examples=5000, stateful_step_count=400
+    # # (e.g. 2863 passing examples, 3 failing examples, 565 invalid examples)
+    # if len(expr.args) > 4 and isinstance(expr.args[1], nodes.Multiply) and isinstance(expr.args[2], nodes.Divide) and isinstance(expr.args[3], nodes.Add):
+    #     raise Exception("OMG can you even make this bro!", expr)
+
     return reduce(operator.mul, (visit(arg) for arg in expr.args))
 
 
@@ -59,4 +65,82 @@ def visit_subtract(expr):
 
 @visit.register(nodes.Divide)
 def visit_divide(expr):
+    # if isinstance(expr.lhs, nodes.Divide) and isinstance(expr.rhs, nodes.Divide):
+    #     raise Exception("Three divides", expr)
+
+    # found in 32sec with mirror or with flip first non-full node
+    if (
+        isinstance(expr.lhs, nodes.Divide) and
+        isinstance(expr.lhs.lhs, nodes.Divide) and
+        isinstance(expr.lhs.rhs, nodes.Divide) and
+        isinstance(expr.rhs, nodes.Divide) and
+        isinstance(expr.rhs.lhs, nodes.Divide) and
+        isinstance(expr.rhs.rhs, nodes.Divide)
+    ):
+        raise Exception("Seven divides", expr)
+
+    # # definitely findable with flip & reflect
+    # if (
+    #     isinstance(expr.lhs, nodes.Divide) and
+    #     isinstance(expr.lhs.lhs, nodes.Divide) and
+    #     isinstance(expr.lhs.rhs, nodes.Divide) and
+    #     isinstance(expr.lhs.lhs.lhs, nodes.Divide) and
+    #     isinstance(expr.lhs.rhs.rhs, nodes.Divide) and
+    #     isinstance(expr.rhs, nodes.Divide) and
+    #     isinstance(expr.rhs.lhs, nodes.Divide) and
+    #     isinstance(expr.rhs.lhs.lhs, nodes.Divide) and
+    #     isinstance(expr.rhs.lhs.rhs, nodes.Divide) and
+    #     isinstance(expr.rhs.rhs, nodes.Divide) and
+    #     # this was findable when starting from seven divides
+    #     # I assume it would be starting from a Value, just with more search
+    #     isinstance(expr.rhs.rhs.lhs, nodes.Divide) and
+    #     isinstance(expr.rhs.rhs.rhs, nodes.Divide)
+    # ):
+    #     raise Exception("Thirteen divides", expr)
+
+    # this is not findable with flip & reflect
+    if (
+        isinstance(expr.lhs, nodes.Divide) and
+        isinstance(expr.lhs.lhs, nodes.Divide) and
+        isinstance(expr.lhs.rhs, nodes.Divide) and
+        isinstance(expr.lhs.lhs.lhs, nodes.Divide) and
+        # ? I think this is unfindable
+        # isinstance(expr.lhs.lhs.rhs, nodes.Divide) and
+        # ? I think this is unfindable
+        # isinstance(expr.lhs.rhs.lhs, nodes.Divide) and
+        isinstance(expr.lhs.rhs.rhs, nodes.Divide) and
+        isinstance(expr.rhs, nodes.Divide) and
+        isinstance(expr.rhs.lhs, nodes.Divide) and
+        isinstance(expr.rhs.rhs, nodes.Divide) and
+        isinstance(expr.rhs.lhs.lhs, nodes.Divide) and
+        isinstance(expr.rhs.lhs.rhs, nodes.Divide) and
+        # this (RRL) is hard but findable:
+        # ~2k examples of depth 200 from a balanced tree with 7 Divide nodes
+        # ~7k examples of depth 200 from a Value (15mins) (Divide nodes only)
+        isinstance(expr.rhs.rhs.lhs, nodes.Divide) and
+        isinstance(expr.rhs.rhs.rhs, nodes.Divide)
+    ):
+        raise Exception("wip divides", expr)
+
+    # # yeah, erm, couldn't find in 20mins with mirror
+    # # can we find this with flip_first_non_full_lhs()?
+    # if (
+    #     isinstance(expr.lhs, nodes.Divide) and
+    #     isinstance(expr.lhs.lhs, nodes.Divide) and
+    #     isinstance(expr.lhs.rhs, nodes.Divide) and
+    #     isinstance(expr.lhs.lhs.lhs, nodes.Divide) and
+    #     isinstance(expr.lhs.lhs.rhs, nodes.Divide) and
+    #     isinstance(expr.lhs.rhs.lhs, nodes.Divide) and
+    #     isinstance(expr.lhs.rhs.rhs, nodes.Divide) and
+    #     isinstance(expr.rhs, nodes.Divide) and
+    #     isinstance(expr.rhs.lhs, nodes.Divide) and
+    #     isinstance(expr.rhs.rhs, nodes.Divide) and
+    #     isinstance(expr.rhs.lhs.lhs, nodes.Divide) and
+    #     isinstance(expr.rhs.lhs.rhs, nodes.Divide) and
+    #     isinstance(expr.rhs.rhs.lhs, nodes.Divide) and
+    #     isinstance(expr.rhs.rhs.rhs, nodes.Divide)
+    # ):
+    #     raise Exception("Fifteen divides", expr)
+
+
     return visit(expr.lhs) / visit(expr.rhs)
--- calculator/tests/test_hypothesis_head.py
+++ calculator/tests/test_hypothesis_head.py
@@ -1,6 +1,6 @@
 from hypothesis.stateful import initialize, invariant, rule, precondition, RuleBasedStateMachine
 import hypothesis.strategies as st
-from hypothesis import assume, settings
+from hypothesis import assume, event, settings, Verbosity
 import pytest
 
 from calculator.nodes import Add, Multiply, Subtract, Divide, Value, NaryExpr, BinaryExpr
@@ -13,13 +13,32 @@ from calculator.calculator2 import calculate as c2
 
 # setting deadline=None because some tests showed timing variability
 # "Unreliable test timings! On an initial run, this test took 325.93ms, which exceeded the deadline of 200.00ms, but on a subsequent run it took 5.95 ms, which did not."
-@settings(max_examples=5000, deadline=None)
+@settings(
+    max_examples=10000,
+    stateful_step_count=200,
+    deadline=None,
+    # verbosity=Verbosity.debug
+)
 class CalculatorMachine(RuleBasedStateMachine):
 
     @initialize(value=st.integers(min_value=0, max_value=9))
     def create_root_node(self, value):
+        # select which types of Nodes to investigate
+        self.enable_binary = True
+        self.enable_nary = True
+
+        # start with a value
         self.expr = Value(value)
 
+        # # start with a binary tree of 7 binary nodes
+        # self.expr = Divide(
+        #     Divide(Divide(Value(0), Value(1)),Divide(Value(1), Value(2))),
+        #     Divide(Divide(Value(2), Value(3)),Divide(Value(3), Value(4)))
+        # )
+
+    # --- Nary section ---
+
+    @precondition(lambda self: self.enable_nary)
     @precondition(lambda self: isinstance(self.expr, NaryExpr))
     @rule(
         value=st.integers(min_value=10, max_value=19),
@@ -27,16 +46,44 @@ class CalculatorMachine(RuleBasedStateMachine):
     def add_value_to_an_nary_node(self, value):
         self.expr.args.append(Value(value))
 
+    # # TODO
+    # @precondition(lambda self: isinstance(self.expr, NaryExpr))
+    # @rule(
+    #     value=st.integers(min_value=40, max_value=49),
+    #     nary=st.booleans(),
+    #     function_choice=st.booleans(),
+    # )
+    # def mutate_leftmost_param_nary_node(self, value, nary, function_choice):
+    #     node_type = type(self.expr)
+    #     if nary:
+    #         if function_choice:
+    #             # TOOD: add a value here?
+    #             new_node = Add([self.expr.args[0]])
+    #         else:
+    #             new_node = Multiply([self.expr.args[0]])
+    #     else:
+    #         if function_choice:
+    #             new_node = Divide(self.expr.args[0], Value(value))
+    #         else:
+    #             new_node = Subtract(self.expr.args[0], Value(value))
+    #     new_args = [new_node]
+    #     if len(self.expr.args) > 1:
+    #         new_args.extend(self.expr.args[1:])
+    #     new_expr = node_type(new_args)
+    #     # raise Exception(self.expr, new_expr)
+    #     self.expr = new_expr
+
+    @precondition(lambda self: self.enable_nary)
     @rule(
-        value=st.integers(min_value=20, max_value=29),
         add=st.booleans(),
     )
-    def add_a_nary_node(self, value, add):
+    def add_a_nary_node(self, add):
         if add:
-            self.expr = Add([self.expr,Value(value)])
+            self.expr = Add([self.expr])
         else:
-            self.expr = Multiply([self.expr,Value(value)])
+            self.expr = Multiply([self.expr])
 
+    @precondition(lambda self: self.enable_nary)
     @precondition(lambda self: isinstance(self.expr, NaryExpr))
     @rule()
     def nary_node_rotate_values(self):
@@ -46,16 +93,23 @@ class CalculatorMachine(RuleBasedStateMachine):
         newlist.append(self.expr.args[0])
         self.expr = node_type(newlist)
 
+
+    # ---Binary section---
+
+    @precondition(lambda self: self.enable_binary)
     @rule(
         value=st.integers(min_value=30, max_value=39),
-        divide=st.booleans(),
+        # divide=st.booleans(),
     )
-    def add_a_binary_node(self, value, divide):
+    # def add_a_binary_node(self, value, divide):
+    def add_a_binary_node(self, value):
+        divide = True
         if divide:
             self.expr = (Divide(self.expr,Value(value)))
         else:
             self.expr = (Subtract(self.expr,Value(value)))
 
+    @precondition(lambda self: self.enable_binary)
     @precondition(lambda self: isinstance(self.expr, BinaryExpr))
     @rule()
     def binary_node_rotate_values(self):
@@ -64,9 +118,109 @@ class CalculatorMachine(RuleBasedStateMachine):
         rhs = self.expr.rhs
         self.expr = node_type(rhs, lhs)
 
+    # TODO
+    @precondition(lambda self: self.enable_binary)
+    @precondition(lambda self: not isinstance(self.expr, Value))
+    @rule(
+        value=st.integers(min_value=50, max_value=59),
+    )
+    def add_a_binary_node_bottom_left(self, value):
+        current_node = self.expr
+        previous_node = self.expr
+        while not isinstance(current_node, Value):
+            previous_node = current_node
+            if isinstance(current_node, BinaryExpr):
+                current_node = current_node.lhs
+            elif isinstance(current_node, NaryExpr):
+                current_node = current_node.args[0]
+            else:
+                assert False
+        new_node = Divide(current_node, Value(value))
+        previous_node.lhs = new_node
+        event("add_a_binary_node_bottom_left")
+
+
+
+    # # TODO: flip at the first node down the lhs that is not full
+    # # this fixes seven divides but maybe not 15 divides
+    # @precondition(lambda self: isinstance(self.expr, BinaryExpr))
+    # @rule()
+    # def flip_first_non_full_lhs(self):
+    #     def _walk_to_first_non_full_node(node):
+    #         root_type = type(node)
+    #         # TODO: not needed but just in case
+    #         if isinstance(node, Value):
+    #             return node
+    #         # if one of the args is a Value, flip them
+    #         elif isinstance(node.lhs, Value) or isinstance(node.rhs, Value):
+    #             return root_type(node.rhs, node.lhs)
+    #         # else keep walking down the lhs
+    #         else:
+    #             return root_type(_walk_to_first_non_full_node(node.lhs), node.rhs)
+    #     flipped_expr = _walk_to_first_non_full_node(self.expr)
+    #     # raise Exception(self.expr, flipped_expr)
+    #     self.expr = flipped_expr
+
+    # TODO: do a breadth-first search for the Binary Node closest to the root
+    # which is not full 
+
+    # TODO: rotate the leaf nodes (at the leaf Binary level or Value level? or lowest level in a sub-tree?) 
+
+    # --- generic section ---
+
+    # Value is fine, just unnecessary
+    @precondition(lambda self: not isinstance(self.expr, Value))
+    @rule()
+    def reflect_a_structure_recursively(self):
+        def _reflect_a_node(node):
+            root_type = type(node)
+            if isinstance(node, Value):
+                return node
+            elif isinstance(node, BinaryExpr):
+                return root_type(_reflect_a_node(node.rhs), _reflect_a_node(node.lhs))
+            elif isinstance(node, NaryExpr):
+                # TODO: because we can rotate the arguments to an Nary, maybe
+                # we don't need to reflect the args?
+                return root_type(node.args)
+            else:
+                assert False
+        reflected_expr = _reflect_a_node(self.expr)
+        self.expr = reflected_expr
+
+    # --- events ---
+
+    def some_binary_shape_events(self):
+        if (
+            isinstance(self.expr, Divide) and
+            isinstance(self.expr.lhs, Divide) and
+            isinstance(self.expr.rhs, Divide)
+        ):
+            event("Three divides")
+        if (
+            isinstance(self.expr, Divide) and
+            isinstance(self.expr.lhs, Divide) and
+            isinstance(self.expr.lhs.lhs, Divide) and
+            isinstance(self.expr.lhs.rhs, Divide) and
+            isinstance(self.expr.rhs, Divide) and
+            isinstance(self.expr.rhs.lhs, Divide) and
+            isinstance(self.expr.rhs.rhs, Divide)
+        ):
+            event("Seven divides")
+
+
     @invariant()
     def compare_results(self):
-        assert c1(self.expr) == c2(self.expr)
+        if self.enable_binary:
+            self.some_binary_shape_events()
+
+        try:
+            # assert c1(self.expr) == c2(self.expr)
+            # bug is in c1 & is a crash
+            c1(self.expr)
+        except AssertionError as exc:
+            raise Exception(self.expr, exc)
+        except TypeError as exc:
+            raise Exception(self.expr, exc)
 
 TestCalculatorMachine = CalculatorMachine.TestCase
 
