f32d885f82a2046ca0b1b40764d048e37ad4960a
Tom Ward <tomward@fmail.co.uk>
2024-05-23 10:59:24 +0100

    wip

--- calculator/tests/test_hypothesis_head.py
+++ calculator/tests/test_hypothesis_head.py
@@ -13,7 +13,7 @@ from calculator.calculator2 import calculate as c2
 
 # setting deadline=None because some tests showed timing variability
 # "Unreliable test timings! On an initial run, this test took 325.93ms, which exceeded the deadline of 200.00ms, but on a subsequent run it took 5.95 ms, which did not."
-@settings(max_examples=5000, deadline=None)
+@settings(max_examples=500, stateful_step_count=400, deadline=None)
 class CalculatorMachine(RuleBasedStateMachine):
 
     @initialize(value=st.integers(min_value=0, max_value=9))
@@ -26,6 +26,33 @@ class CalculatorMachine(RuleBasedStateMachine):
     )
     def add_value_to_an_nary_node(self, value):
         self.expr.args.append(Value(value))
+
+    # TODO
+    @precondition(lambda self: isinstance(self.expr, NaryExpr))
+    @rule(
+        value=st.integers(min_value=40, max_value=49),
+        nary=st.booleans(),
+        function_choice=st.booleans(),
+    )
+    def mutate_leftmost_param_nary_node(self, value, nary, function_choice):
+        node_type = type(self.expr)
+        if nary:
+            if function_choice:
+                # TOOD: add a value here?
+                new_node = Add([self.expr.args[0]])
+            else:
+                new_node = Multiply([self.expr.args[0]])
+        else:
+            if function_choice:
+                new_node = Divide(self.expr.args[0], Value(value))
+            else:
+                new_node = Subtract(self.expr.args[0], Value(value))
+        new_args = [new_node]
+        if len(self.expr.args) > 1:
+            new_args.extend(self.expr.args[1:])
+        new_expr = node_type(new_args)
+        # raise Exception(self.expr, new_expr)
+        self.expr = new_expr
 
     @rule(
         value=st.integers(min_value=20, max_value=29),
@@ -64,9 +91,19 @@ class CalculatorMachine(RuleBasedStateMachine):
         rhs = self.expr.rhs
         self.expr = node_type(rhs, lhs)
 
+    # TODO
+    # def mutate_leftmost_param_binary_node(self):
+
     @invariant()
     def compare_results(self):
-        assert c1(self.expr) == c2(self.expr)
+        try:
+            # assert c1(self.expr) == c2(self.expr)
+            # bug is in c1 & is a crash
+            c1(self.expr)
+        except AssertionError as exc:
+            raise Exception(self.expr, exc)
+        except TypeError as exc:
+            raise Exception(self.expr, exc)
 
 TestCalculatorMachine = CalculatorMachine.TestCase
 
