8de30b9760b6e8a2d667cfafff233c1a596daffb
Tom Ward <tomward@fmail.co.uk>
2024-05-25 15:09:12 +0100

    Implement add nary node to bottom left
    * also refactor a bit

--- calculator/tests/test_hypothesis_head.py
+++ calculator/tests/test_hypothesis_head.py
@@ -46,32 +46,36 @@ class CalculatorMachine(RuleBasedStateMachine):
     def add_value_to_an_nary_node(self, value):
         self.expr.args.append(Value(value))
 
-    # # TODO
-    # @precondition(lambda self: isinstance(self.expr, NaryExpr))
-    # @rule(
-    #     value=st.integers(min_value=40, max_value=49),
-    #     nary=st.booleans(),
-    #     function_choice=st.booleans(),
-    # )
-    # def mutate_leftmost_param_nary_node(self, value, nary, function_choice):
-    #     node_type = type(self.expr)
-    #     if nary:
-    #         if function_choice:
-    #             # TOOD: add a value here?
-    #             new_node = Add([self.expr.args[0]])
-    #         else:
-    #             new_node = Multiply([self.expr.args[0]])
-    #     else:
-    #         if function_choice:
-    #             new_node = Divide(self.expr.args[0], Value(value))
-    #         else:
-    #             new_node = Subtract(self.expr.args[0], Value(value))
-    #     new_args = [new_node]
-    #     if len(self.expr.args) > 1:
-    #         new_args.extend(self.expr.args[1:])
-    #     new_expr = node_type(new_args)
-    #     # raise Exception(self.expr, new_expr)
-    #     self.expr = new_expr
+    def walk_to_bottom_left_expr(expr):
+        current_node = self.expr
+        previous_node = self.expr
+        while not isinstance(current_node, Value):
+            previous_node = current_node
+            if isinstance(current_node, BinaryExpr):
+                current_node = current_node.lhs
+            elif isinstance(current_node, NaryExpr):
+                current_node = current_node.args[0]
+            else:
+                assert False
+        return (current_node, previous_node)
+
+    @precondition(lambda self: self.enable_nary)
+    @precondition(lambda self: not isinstance(self.expr, Value))
+    @rule(
+        value=st.integers(min_value=60, max_value=69),
+    )
+    def add_a_nary_node_bottom_left(self, value):
+        current_node, previous_node = walk_to_bottom_left_expr(self.expr)
+
+        new_node = Add([current_node, Value(value)])
+
+        if isinstance(previous_node, BinaryExpr):
+            previous_node.lhs = new_node
+        elif isinstance(previous_node, NaryExpr):
+            previous_node.args[0] = new_node
+        else:
+            assert False
+        event("add_a_nary_node_bottom_left")
 
     @precondition(lambda self: self.enable_nary)
     @rule(
@@ -101,8 +105,8 @@ class CalculatorMachine(RuleBasedStateMachine):
         value=st.integers(min_value=30, max_value=39),
         # divide=st.booleans(),
     )
-    # def add_a_binary_node(self, value, divide):
-    def add_a_binary_node(self, value):
+    # def add_a_binary_node_head(self, value, divide):
+    def add_a_binary_node_head(self, value):
         divide = True
         if divide:
             self.expr = (Divide(self.expr,Value(value)))
@@ -118,25 +122,22 @@ class CalculatorMachine(RuleBasedStateMachine):
         rhs = self.expr.rhs
         self.expr = node_type(rhs, lhs)
 
-    # TODO
     @precondition(lambda self: self.enable_binary)
     @precondition(lambda self: not isinstance(self.expr, Value))
     @rule(
         value=st.integers(min_value=50, max_value=59),
     )
     def add_a_binary_node_bottom_left(self, value):
-        current_node = self.expr
-        previous_node = self.expr
-        while not isinstance(current_node, Value):
-            previous_node = current_node
-            if isinstance(current_node, BinaryExpr):
-                current_node = current_node.lhs
-            elif isinstance(current_node, NaryExpr):
-                current_node = current_node.args[0]
-            else:
-                assert False
+        current_node, previous_node = walk_to_bottom_left_expr(self.expr)
+
         new_node = Divide(current_node, Value(value))
-        previous_node.lhs = new_node
+
+        if isinstance(previous_node, BinaryExpr):
+            previous_node.lhs = new_node
+        elif isinstance(previous_node, NaryExpr):
+            previous_node.args[0] = new_node
+        else:
+            assert False
         event("add_a_binary_node_bottom_left")
 
 
